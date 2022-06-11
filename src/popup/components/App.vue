381b32a7410e6f2cb3ddad36a1002af7b5e17938
yangluoUp <190672460@qq.com>
2022-06-12 00:34:34 +0800

    整合elemetui

---
+++ src/popup/components/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <el-table
+    ref="multipleTableRef"
+    :data="tableData"
+    style="width: 1000px"
+    max-height="250"
+    @selection-change="handleSelectionChange"
+  >
+    <el-table-column type="selection" width="55" />
+    <el-table-column label="Date" width="120">
+      <!-- <template #default="scope">{{ scope.row.date }}</template> -->
+    </el-table-column>
+    <el-table-column property="name" label="Name" width="120" />
+    <el-table-column property="address" label="Address" show-overflow-tooltip />
+  </el-table>
+  <div style="margin-top: 20px">
+    <el-button @click="toggleSelection([tableData[1], tableData[2]])"
+      >Toggle selection status of second and third rows</el-button
+    >
+    <el-button @click="toggleSelection()">Clear selection</el-button>
+  </div>
+</template>
+
+<script>
+
+import { ref } from 'vue'
+import { ElTable } from 'element-plus'
+
+export default {
+  data(){
+    return {
+      tableData: [
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-08',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-06',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-07',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }
+      ],
+      multipleSelection: []
+    }
+  },
+  methods: {
+      toggleSelection(rows) {
+        if (rows) {
+          rows.forEach(row => {
+            this.$refs.multipleTableRef.toggleRowSelection(row);
+          });
+        } else {
+          this.$refs.multipleTableRef.clearSelection();
+        }
+      },
+      handleSelectionChange(val) {
+        this.multipleSelection = val;
+      }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.popup-header {
+  width: 100px;
+  height: 100px;
+  color: red;
+}
+</style>
